5f05bb4eaa6076959f3ea24953eaff559307f95b
Edgar Lopes <edgar.lopes@synchronoss.com>
2017-01-16 19:22:07 +0000

    UI elements. Custom cells for table and collection views

---
+++ WeatherApp/WeatherApp/ViewContollers/WaetherWeekCell.swift
@@ -0,0 +1,15 @@
+//
+//  WaetherWeekCell.swift
+//  WeatherApp
+//
+//  Created by Edgar Lopes on 15/01/2017.
+//  Copyright © 2017 Edgar Lopes. All rights reserved.
+//
+
+import UIKit
+class WaetherWeekCell: UICollectionViewCell {
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var dayInWords: UILabel!
+    @IBOutlet weak var range: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+}
---
+++ WeatherApp/WeatherApp/ViewContollers/WeatherDailyCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherDailyCell.swift
+//  WeatherApp
+//
+//  Created by Edgar Lopes on 14/01/2017.
+//  Copyright © 2017 Edgar Lopes. All rights reserved.
+//
+
+import UIKit
+
+class WeatherDailyCell: UITableViewCell {
+    @IBOutlet weak var day: UILabel!
+    @IBOutlet weak var temperature: UILabel!
+    @IBOutlet weak var icon: UIImageView!
+}
